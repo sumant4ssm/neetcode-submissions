12c67b0d697a5279b119f5d04811811c143fee33
sumant4ssm <sumant4ssm@gmail.com>
2026-05-19 12:20:47 +0530

    Add: python-dynamic-typing - submission-1

---
+++ Python For Beginners/python-dynamic-typing/submission-1.py
@@ -0,0 +1,10 @@
+variable = 1
+print(type(variable))
+variable =1.2
+print(type(variable))
+variable =True
+print(type(variable))
+variable = "abc"
+print(type(variable))
+variable = [1,2,3]
+print(type(variable))
